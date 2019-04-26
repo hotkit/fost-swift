083b9ae00c5178b420f5ed39a5156a4e410cf717
Kirit Sælensminde <kirit@kirit.com>
2019-04-26 13:31:31 +0700

    This empty file compiles as part of the cmake section in the parent project

---
+++ include/fost/swift/settings.hpp
@@ -0,0 +1,25 @@
+/**
+    Copyright 2019, Felspar Co Ltd. <https://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#pragma once
+
+#ifndef settings_hpp
+#define settings_hpp
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
