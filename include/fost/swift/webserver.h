f68430378cc681c3f7e413b4cdc860be97174708
Kirit Sælensminde <kirit@kirit.com>
2019-06-20 13:52:04 +0700

    We are going to need to be able to start and stop the webserver

---
+++ include/fost/swift/webserver.h
@@ -0,0 +1,25 @@
+/**
+    Copyright 2019, Felspar Co Ltd. <https://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#pragma once
+
+#ifndef fost_swift_webserver_hpp
+#define fost_swift_webserver_hpp
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
