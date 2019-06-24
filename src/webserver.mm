2c389e26b38a95dcc0a3eaaf6bc18c6620d32c4c
Kirit Sælensminde <kirit@kirit.com>
2019-06-24 11:17:40 +0700

    Add asset loading view that doesn't yet use the callback

--- src/webserver.mm
+++ src/webserver.mm
@@ -33,3 +33,70 @@ extern "C" void webserver_start() {
 extern "C" void register_file_loader(file_loader_callback fp) {
     file_loader = fp;
 }
+
+
+namespace {
+
+
+    /**
+     * Returns data using the callback, or an empty buffer if not found.
+      */
+    f5::shared_buffer<unsigned char> assetdata(const fostlib::string path) {
+        return f5::shared_buffer<unsigned char>();
+    }
+
+
+    /**
+     * Returns the correct response for the provided data.
+     */
+    std::pair<boost::shared_ptr<fostlib::mime>, int> assetresponse(
+            f5::shared_buffer<unsigned char> bytes,
+            const fostlib::string path
+    ) {
+        if ( bytes.data() ) {
+            return std::make_pair(
+                    boost::shared_ptr<fostlib::mime>(
+                            new fostlib::binary_body(
+                                    bytes.begin(), bytes.end(),
+                                    fostlib::mime::mime_headers(),
+                                    fostlib::urlhandler::mime_type(
+                                            fostlib::coerce<fostlib::fs::path>(path)))),
+                    200);
+        } else {
+            return std::make_pair(
+                    boost::shared_ptr<fostlib::mime>(
+                            new fostlib::text_body(
+                                    "<html><head><title>404</title></head>"
+                                            "<body><h1>404</h1><pre>" + path + "</pre></body></html>")),
+                    404);
+        }
+    }
+
+
+    /**
+     * View that assumes a directory structure is available in the to be loaded
+     * by the callback. Handles working out the correct path to find the file at.
+     */
+    const class assets : public fostlib::urlhandler::view {
+    public:
+        assets()
+        : view("fost.ios.assets") {
+        }
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
+            const fostlib::json &config, const fostlib::string &requested_path,
+            fostlib::http::server::request &req,
+            const fostlib::host &
+        ) const {
+            fostlib::string path(
+                fostlib::coerce<fostlib::string>(config["asset"]) + requested_path);
+            if ( requested_path.empty() || requested_path.endswith("/") ) {
+                /// **TODO** This filename should be read from the configuration
+                path += "index.html";
+            }
+            return assetresponse(assetdata(path), path);
+        }
+    } c_assets_view;
+
+
+}
